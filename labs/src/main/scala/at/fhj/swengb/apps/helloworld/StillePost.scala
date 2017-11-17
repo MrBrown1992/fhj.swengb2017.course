7dcb3f38997910d5b36d77b5d7f988a96ae9926f
malichri <m4lcha@gmail.com>
2017-11-17 15:00:48 +0100

    Stille Post -> add entry

--- labs/src/main/scala/at/fhj/swengb/apps/helloworld/StillePost.scala
+++ labs/src/main/scala/at/fhj/swengb/apps/helloworld/StillePost.scala
@@ -3,5 +3,6 @@ package at.fhj.swengb.apps.helloworld
 object StillePost {
   def main(args: Array[String]): Unit = {
     println("Erste Message: Liebe Grüße!")
+    println("Umständlichstes stille Post ever")
   }
 }
