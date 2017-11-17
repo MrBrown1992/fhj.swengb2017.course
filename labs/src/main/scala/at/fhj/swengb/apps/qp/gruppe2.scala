8dc48450776a3b4ea00766a3e10cf9cbf82bed09
Susanne Hütter <huettersue@gmail.com>
2017-11-17 15:07:53 +0100

    Stille Post teil 2, wort 3

--- labs/src/main/scala/at/fhj/swengb/apps/qp/gruppe2.scala
+++ labs/src/main/scala/at/fhj/swengb/apps/qp/gruppe2.scala
@@ -3,7 +3,8 @@ package at.fhj.swengb.apps.qp
 object gruppe2 {
   def main(args: Array[String]): Unit = {
     println("Die FH ")
-    println(" hat ")
+    println("hat ")
+    println("heute ")
   }
 
 }
